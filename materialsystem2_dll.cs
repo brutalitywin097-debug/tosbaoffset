6437199befbc6ed82171eac284886bc0a2d8c435
brutalitywin097-debug <brutalitywin097@gmail.com>
2025-12-09 21:37:56 +0300

    Add files via upload

--- materialsystem2_dll.cs
+++ materialsystem2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-12-08 15:46:17.278140700 UTC
+// 2025-12-09 18:36:39.218329100 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: materialsystem2.dll
--- pulse_system_dll.rs
+++ pulse_system_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-12-08 15:46:17.278140700 UTC
+// 2025-12-09 18:36:39.218329100 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
