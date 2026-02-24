e49bc54d7ec54d82f3d7dc8300b0a126f54151f9
brutalitywin097-debug <brutalitywin097@gmail.com>
2026-02-24 16:58:40 +0300

    Add files via upload

--- panorama_dll.cs
+++ panorama_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-02-10 17:36:01.057721300 UTC
+// 2026-02-24 01:42:29.356100200 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: panorama.dll
--- rendersystemdx11_dll.rs
+++ rendersystemdx11_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-02-10 17:36:01.057721300 UTC
+// 2026-02-24 01:42:29.356100200 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
