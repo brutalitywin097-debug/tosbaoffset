47b826cc002b896bac1334b9cc0d706b311a283e
brutalitywin097-debug <brutalitywin097@gmail.com>
2026-05-08 05:26:10 +0300

    Add files via upload

--- animationsystem_dll.cs
+++ animationsystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-04-30 23:00:15.075673400 UTC
+// 2026-05-08 02:23:51.973519 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: animationsystem.dll
