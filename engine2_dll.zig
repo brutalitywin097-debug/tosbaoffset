350a9173e8085589fb7d110e1907635b5303c7cc
brutalitywin097-debug <brutalitywin097@gmail.com>
2026-05-21 12:57:04 +0300

    Add files via upload

--- engine2_dll.zig
+++ engine2_dll.zig
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-04-28 22:45:24.236403600 UTC
+// 2026-05-20 23:19:26.948644500 UTC
 
 pub const cs2_dumper = struct {
     pub const schemas = struct {
--- particles_dll.rs
+++ particles_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-19 02:16:40.812841100 UTC
+// 2026-05-20 23:19:26.948644500 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
