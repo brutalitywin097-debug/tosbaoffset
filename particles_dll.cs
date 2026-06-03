aadc5f4c6f5fd4225d6fb1f8232f1889d3525a12
brutalitywin097-debug <brutalitywin097@gmail.com>
2026-06-03 22:04:07 +0300

    Add files via upload

--- particles_dll.cs
+++ particles_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-06-02 14:37:10.876359900 UTC
+// 2026-06-03 17:30:50.803562 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: particles.dll
