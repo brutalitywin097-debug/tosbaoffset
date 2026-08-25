060657aec6bb9aa2067fa9566b8552bd53cf9d1c
brutalitywin097-debug <brutalitywin097@gmail.com>
2026-08-25 03:31:59 +0300

    Add files via upload

--- buttons.rs
+++ buttons.rs
@@ -1,26 +1,26 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-13 00:19:04.393840300 UTC
+// 2026-08-25 00:16:20.249453 UTC
 
 #![allow(non_upper_case_globals, unused)]
 
 pub mod cs2_dumper {
     // Module: client.dll
     pub mod buttons {
-        pub const attack: usize = 0x2099000;
-        pub const attack2: usize = 0x2099090;
-        pub const back: usize = 0x20992D0;
-        pub const duck: usize = 0x20995A0;
-        pub const forward: usize = 0x2099240;
-        pub const jump: usize = 0x2099510;
-        pub const left: usize = 0x2099360;
-        pub const lookatweapon: usize = 0x23BEA40;
-        pub const reload: usize = 0x2098F70;
-        pub const right: usize = 0x20993F0;
-        pub const showscores: usize = 0x23BE920;
-        pub const sprint: usize = 0x2098EE0;
-        pub const turnleft: usize = 0x2099120;
-        pub const turnright: usize = 0x20991B0;
-        pub const r#use: usize = 0x2099480;
-        pub const zoom: usize = 0x23BE9B0;
+        pub const attack: usize = 0x20B48F0;
+        pub const attack2: usize = 0x20B4980;
+        pub const back: usize = 0x20B4BC0;
+        pub const duck: usize = 0x20B4E90;
+        pub const forward: usize = 0x20B4B30;
+        pub const jump: usize = 0x20B4E00;
+        pub const left: usize = 0x20B4C50;
+        pub const lookatweapon: usize = 0x23DCBA0;
+        pub const reload: usize = 0x20B4860;
+        pub const right: usize = 0x20B4CE0;
+        pub const showscores: usize = 0x23DCA80;
+        pub const sprint: usize = 0x20B47D0;
+        pub const turnleft: usize = 0x20B4A10;
+        pub const turnright: usize = 0x20B4AA0;
+        pub const r#use: usize = 0x20B4D70;
+        pub const zoom: usize = 0x23DCB10;
     }
 }
